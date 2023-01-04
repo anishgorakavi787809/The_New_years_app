2c4f62cddfa01e96c95898166d6b9c1cb1566b0a
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2023-01-04 15:04:12 -0600

    dd

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import uvicorn
+import openai
+import os
+from pydantic import BaseModel
+
+openai.api_key = os.environ["FOO"]
+app = FastAPI()
+model_engine = "text-davinci-003"
+
+
+class Proquest(BaseModel):
+    prompt: str
+
+
+@app.get("/")
+def wow():
+    return {"mes":"WOW!"}
+
+
+@app.post("/ask")
+async def asker(arg:Proquest):
+    prompter = openai.Completion.create(engine=model_engine,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+     prompt=arg.prompt)
+
+    return {"He said": prompter.choices[0]["text"]}
+
+uvicorn.run(app, port=5000)
